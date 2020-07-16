d4b8d2866d7eec315f9baf968c820cec3e75e77b
EvertonBorges <evertonsoaresborges@gmail.com>
2020-07-15 21:07:14 -0400

    feat - Camera seguindo jogador

---
+++ Assets/Scripts/ControlaCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaCamera : MonoBehaviour
+{
+
+    public GameObject jogador;
+
+    Vector3 distCompensar;
+    
+    void Start()
+    {
+        distCompensar = transform.position - jogador.transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = jogador.transform.position + distCompensar;
+    }
+
+}

62074beca68768a382affe8ddf9626acc0685c7a
EvertonBorges <evertonsoaresborges@gmail.com>
2020-07-21 19:15:44 -0400

    feat - Atirando

---
+++ Assets/Scripts/Bala.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bala : MonoBehaviour
+{
+	
+	[SerializeField]
+	private float velocidade = 20;
+	
+	private Rigidbody _rigidbody;
+	
+	void Start()
+	{
+		_rigidbody = GetComponent<Rigidbody>();
+	}
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _rigidbody.MovePosition(_rigidbody.position + transform.forward * velocidade * Time.deltaTime);
+    }
+}

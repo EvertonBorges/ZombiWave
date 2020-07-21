35885e0c0c63fe1500c88f6025a61c54bb08c429
EvertonBorges <evertonsoaresborges@gmail.com>
2020-07-21 18:43:07 -0400

    feat - Zombi seguindo o jogador

---
+++ Assets/Scripts/ControlaInimigo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaInimigo : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject jogador;
+
+    [SerializeField]
+    private float velocidade = 5;
+
+    private Rigidbody _rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 direcao = jogador.transform.position - transform.position;
+        _rigidbody.MovePosition(_rigidbody.position + direcao.normalized * velocidade * Time.deltaTime);
+    }
+}

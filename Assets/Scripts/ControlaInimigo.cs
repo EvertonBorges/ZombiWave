94a6b08c1879bd552f1aeae84898160c96dfa6fe
EvertonBorges <evertonsoaresborges@gmail.com>
2020-07-21 21:57:17 -0400

    feat - Inimigos atacando

--- Assets/Scripts/ControlaInimigo.cs
+++ Assets/Scripts/ControlaInimigo.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ControlaInimigo : MonoBehaviour
 {
+    private const string ANIMATOR_ATACANDO = "Atacando";
 
     [SerializeField]
     private GameObject jogador;
@@ -12,11 +13,13 @@ public class ControlaInimigo : MonoBehaviour
     private float velocidade = 5;
 
     private Rigidbody _rigidbody;
+    private Animator _animator;
 
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -28,14 +31,18 @@ public class ControlaInimigo : MonoBehaviour
     void FixedUpdate()
     {
 		float distancia = Vector3.Distance(transform.position, jogador.transform.position);
+		Vector3 direcao = jogador.transform.position - transform.position;
 		
+		Quaternion rotacao = Quaternion.LookRotation(direcao);
+		_rigidbody.MoveRotation(rotacao);
+
 		if (distancia > 2.5)
 		{
-			Vector3 direcao = jogador.transform.position - transform.position;
 			_rigidbody.MovePosition(_rigidbody.position + direcao.normalized * velocidade * Time.deltaTime);
-			
-			Quaternion rotacao = Quaternion.LookRotation(direcao);
-			_rigidbody.MoveRotation(rotacao);
-		}
+            _animator.SetBool(ANIMATOR_ATACANDO, false);
+		} else
+        {
+            _animator.SetBool(ANIMATOR_ATACANDO, true);
+        }
     }
 }

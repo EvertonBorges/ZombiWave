e802a5c919dd3896de9be9bfae3503b759da94d2
EvertonBorges <evertonsoaresborges@gmail.com>
2020-07-21 21:46:07 -0400

    feat - Rotação do jogador

--- Assets/Scripts/ControlaJogador.cs
+++ Assets/Scripts/ControlaJogador.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class ControlaJogador : MonoBehaviour
 {
 
-    public float velocidade = 10;
+    [SerializeField]
+    private LayerMask mascaraChao;
+
+    [SerializeField]
+    private float velocidade = 10;
 
     private Vector3 direcao;
 
@@ -32,6 +36,20 @@ public class ControlaJogador : MonoBehaviour
     void FixedUpdate()
     {
         _rigidbody.MovePosition(_rigidbody.position + direcao * velocidade * Time.deltaTime);
+
+        Ray raio = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(raio.origin, raio.direction * 100, Color.red);
+
+        RaycastHit impacto;
+
+        if (Physics.Raycast(raio, out impacto, 100, mascaraChao))
+        {
+            Vector3 posicaoMiraJogador = impacto.point - transform.position;
+            posicaoMiraJogador.y = transform.position.y;
+
+            Quaternion rotacao = Quaternion.LookRotation(posicaoMiraJogador);
+            _rigidbody.MoveRotation(rotacao);
+        }
     }
 
 }

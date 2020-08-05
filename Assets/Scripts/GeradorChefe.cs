b47df080bb7a1f2118d2bfc997246b97678613cd
EvertonBorges <evertonsoaresborges@gmail.com>
2020-08-04 22:52:35 -0400

    feat - Gerador de chefe

---
+++ Assets/Scripts/GeradorChefe.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeradorChefe : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject chefePrefab;
+
+    [SerializeField]
+    private float tempoEntreGeracoes = 60;
+
+    private float _tempoParaProximaGeracao = 0f;
+
+    private void Start()
+    {
+        _tempoParaProximaGeracao = tempoEntreGeracoes;
+    }
+
+    private void Update()
+    {
+        if(Time.timeSinceLevelLoad > _tempoParaProximaGeracao)
+        {
+            Instantiate(chefePrefab, transform.position, Quaternion.identity);
+            _tempoParaProximaGeracao = Time.timeSinceLevelLoad + tempoEntreGeracoes;
+        }
+    }
+
+}

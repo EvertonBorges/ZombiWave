d3f98c4ee538bd74630ddeed7405e8991097e166
EvertonBorges <evertonsoaresborges@gmail.com>
2020-08-06 20:28:10 -0400

    feat - Posição de geração do Chefe

--- Assets/Scripts/GeradorChefe.cs
+++ Assets/Scripts/GeradorChefe.cs
@@ -11,23 +11,47 @@ public class GeradorChefe : MonoBehaviour
     [SerializeField]
     private float tempoEntreGeracoes = 60;
 
+    [SerializeField]
+    private Transform[] posicoesPossiveisDeGeracao;
+
     private float _tempoParaProximaGeracao = 0f;
     private ControlaInterface _scriptControlaInterface;
+    private Transform _jogador;
 
     private void Start()
     {
         _tempoParaProximaGeracao = tempoEntreGeracoes;
         _scriptControlaInterface = GameObject.FindObjectOfType(typeof(ControlaInterface)) as ControlaInterface;
+        _jogador = GameObject.FindGameObjectWithTag(Tags.JOGADOR).transform;
     }
 
     private void Update()
     {
         if(Time.timeSinceLevelLoad > _tempoParaProximaGeracao)
         {
-            Instantiate(chefePrefab, transform.position, Quaternion.identity);
+            Vector3 posicaoDeCriacao = CalcularPosicaoMaisDistanteDoJogador();
+            Instantiate(chefePrefab, posicaoDeCriacao, Quaternion.identity);
             _scriptControlaInterface.AparecerTextoChefeCriado();
             _tempoParaProximaGeracao = Time.timeSinceLevelLoad + tempoEntreGeracoes;
         }
+    }
+
+    private Vector3 CalcularPosicaoMaisDistanteDoJogador()
+    {
+        Vector3 posicaoDeMaiorDistancia = Vector3.zero;
+        float maiorDistancia = 0;
+
+        foreach(Transform posicao in posicoesPossiveisDeGeracao)
+        {
+            float distanciaEntreOJogador = Vector3.Distance(posicao.position, _jogador.position);
+            if (distanciaEntreOJogador > maiorDistancia)
+            {
+                maiorDistancia = distanciaEntreOJogador;
+                posicaoDeMaiorDistancia = posicao.position;
+            }
+        }
+
+        return posicaoDeMaiorDistancia;
     }
 
 }
